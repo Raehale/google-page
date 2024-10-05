90ae932ace3bea22b0a73a92a2cdaf30cba89f27
Raehale <alyssarae98ha@gmail.com>
2024-10-04 23:29:37 -0600

    added image location

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 //gets random photos related to rats
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=rat")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=rats")
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
 		document.getElementById("author").textContent = `By: ${data.user.name}`
+        document.getElementById("imageLocation").textContent = `${data.user.location}`
+        console.log(data.user)
     })
     .catch(err => {
         document.body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1602089108168-41dd3471da06?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3Mjc3OTcxODV8&ixlib=rb-4.0.3&q=80&w=1080)';
