9b77b4af983f846f20f6813002f8d880921d40b4
Raehale <alyssarae98ha@gmail.com>
2024-10-06 14:23:49 -0600

    changes from suggestions

--- unsplashPhotos.js
+++ unsplashPhotos.js
@@ -1,18 +1,17 @@
 //gets random photos related to rats
-export function getBackground() {
-    fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=rats")
-    .then(res => res.json())
-    .then(data => {
+export async function getBackground() {
+    try {
+        const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=rats");
+        const data = await res.json();
         document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
+        document.getElementById("author").textContent = `By: ${data.user.name}`
         if (data.user.location) {
             document.getElementById("imageLocation").textContent = `${data.user.location}`
         }
-    })
-    .catch(err => {
+    } catch(err) {
         document.body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1602089108168-41dd3471da06?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3Mjc3OTcxODV8&ixlib=rb-4.0.3&q=80&w=1080)';
         document.getElementById("author").textContent = 'Zachary Kadolph';
-    })
+    }
 }
 
 //download background image
--- weather.js
+++ weather.js
@@ -1,23 +1,20 @@
 //gets users current weather using their position
 export function getWeather() {
-    navigator.geolocation.getCurrentPosition(position => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial&appid=104bdb29c7eb291568aef67ab2067fe3`)
-            .then(res => {
-                if (!res.ok) {
-                    throw Error("Weather data not available.");
-                }
-    
-                return res.json();
-            })
-            .then(data => {
-                console.log(data)
-                const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-                document.getElementById("weather").innerHTML = `
-                    <img src=${iconUrl} alt="Weather Icon" />
-                    <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-                    <p class="weather-city">${data.name}</p>
-                `;
-            })
-            .catch(err => console.error(err));
+    navigator.geolocation.getCurrentPosition(async position => {
+        try {
+            const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial&appid=104bdb29c7eb291568aef67ab2067fe3`);
+            if (!res.ok) {
+                throw Error("Weather ata not available.");
+            }
+            const data = await res.json();
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} alt="Weather Icon" />
+                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
+            `;
+        } catch(err) {
+            console.log(err);
+        }
     });
 }
